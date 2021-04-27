461658b54d536c77a66e5e2502fba94ff71df5ac
nimamot <nima.motieifard@gmail.com>
2021-04-27 16:22:17 -0400

    e

--- mac_changer.py/mac_changer.py
+++ mac_changer.py/mac_changer.py
@@ -2,9 +2,11 @@
 
 import subprocess
 
-interface = input("what interface would you like to change the macAddress for(eth0/wlan0)?")
-new_macAddress = input("What's the new macAddress(aa:bb:cc:dd:ff:gg)?")
-subprocess.call(f"ifconfig {interface} down", shell=True)
-subprocess.call(f"ifconfig {interface} hw ether 00:11:22:33:44:66", shell=True)
-subprocess.call(f"ifconfig {interface} up", shell=True)
+interface = "wlan0"
+    # input("what interface would you like to change the macAddress for(eth0/wlan0)?")
+new_macAddress = "00:11:22:33:44:77"
+    # input("What's the new macAddress(aa:bb:cc:dd:ff:gg)?")
+subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_macAddress, shell=True)
+subprocess.call("ifconfig " + interface  + " up", shell=True)
 

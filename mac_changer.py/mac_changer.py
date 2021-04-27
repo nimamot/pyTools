f2ac7cb9f92c583bd186e09ab05c709af8838e53
nimamot <nima.motieifard@gmail.com>
2021-04-27 17:15:33 -0400

    added optparse

--- mac_changer.py/mac_changer.py
+++ mac_changer.py/mac_changer.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python3
 
 import subprocess
+import optparse
 
-interface = "wlan0"
-    # input("what interface would you like to change the macAddress for(eth0/wlan0)?")
-new_macAddress = "00:11:22:33:44:77"
-    # input("What's the new macAddress(aa:bb:cc:dd:ff:gg)?")
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_macAddress, shell=True)
-subprocess.call("ifconfig " + interface  + " up", shell=True)
 
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+parser.add_option("-m", "--mac", dest="new_macAddress", help="New MAc Address")
+
+(options, arguments) = parser.parse_args()
+
+
+interface = options.interface
+new_macAddress = options.new_macAddress
+
+
+print("changing mac address for " + interface + "to" + new_macAddress)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_macAddress])
+subprocess.call(["ifconfig", interface, "up"])

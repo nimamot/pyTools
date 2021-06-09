560b6a55ca71ecea920aa81e99d38a60f17aefed
nimamot <64922998+nimamot@users.noreply.github.com>
2021-06-09 12:41:48 -0700

    minor changes

--- binance/binance01.py
+++ binance/binance01.py
@@ -6,5 +6,15 @@ from binance.client import Client
 api_key = input("input your key:")
 api_secret = input("input your secret key:")
 client = Client(api_key, api_secret)
-asset_input = input("asset:")
-print(client.get_asset_balance(asset=asset_input))
+
+action = input("what do you wanna do?(BALANCE / PRICE)")
+
+
+
+def checkBalance():
+    asset_input = input("asset:")
+    print(client.get_asset_balance(asset=asset_input))
+
+
+if action == "BALANCE":
+    checkBalance()

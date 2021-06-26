8e85549985b1d42d905e420de61578c4f26b7f21
nimamot <nima.motieifard@gmail.com>
2021-06-26 00:00:02 -0400

    minor changes

--- network_scanner.py
+++ network_scanner.py
@@ -5,8 +5,16 @@ def scan(ip):
     # Ether() creates an Ether object
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1)
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1)[0]
+
+    for element in answered_list:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+        print("-------------------------------------------------")
+
     print(answered_list.summary())
+
+
 
 
 scan("10.11.12.1/24")

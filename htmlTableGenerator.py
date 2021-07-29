d8b2594a04d42d6b2c899f70d88f42430f33e1e5
nimamot <64922998+nimamot@users.noreply.github.com>
2021-07-29 15:38:52 -0700

    added htmltablegenarator

---
+++ htmlTableGenerator.py
@@ -0,0 +1,23 @@
+rows = input("number of rows")
+columns = input("numbber of columns")
+
+table_header = ""
+table_headers = columns*'<th class="tg-0lax"> </th>'
+
+
+final_code = """
+<style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
+  font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-0lax{text-align:left;vertical-align:top}
+.tg .tg-0pky{border-color:inherit;text-align:left;vertical-align:top}
+</style>
+<table class="tg">
+<thead>
+
+"""
+
+print(final_code)
